e42db25e70a3aaed655d70ac597c1330bacfacad
Izamakzi <izzyzientek1015@gmail.com>
2020-03-31 14:43:52 -0500

    Milestone 1: Issue #2

--- app/src/main/java/com/prioritize/MainActivity.java
+++ app/src/main/java/com/prioritize/MainActivity.java
@@ -1,14 +1,32 @@
 package com.prioritize;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvItems;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvItems = findViewById(R.id.rvItems);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_add);
+            }
+        });
+
+
     }
 }
